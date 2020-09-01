bbdfb80347304f07af81805db9542510f450f4b7
saatwik <saathwikg@gmail.com>
2020-08-31 22:09:38 -0400

    title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My-Service-Worker';
+  title = 'My-Service-Worker-app';
 }
