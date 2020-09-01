b2a93842a862469886e6f8aea17b7ce67c55d794
saatwik <saathwikg@gmail.com>
2020-08-31 22:15:58 -0400

    unit tests fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'service-worker-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('service-worker-app');
+    expect(app.title).toEqual('My-Service-Worker-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('service-worker-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('My-Service-Worker-app app is running!');
   });
 });
